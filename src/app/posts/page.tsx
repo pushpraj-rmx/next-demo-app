a112174c617d256698c1a7c052227090a7fa037b
Pushpraj Dwivedi <pushprajdwivedi001@gmail.com>
2024-04-06 11:59:06 +0530

    init

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1 className='text-4xl font-bold hover:text-red-800'>Posts page</h1>
+    </div>
+  )
+}
+
+export default page
